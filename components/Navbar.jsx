e7ed911209b70258fcb417bb9e45cded9b913774
c7-studio <c7studio.www@gmail.com>
2022-10-18 11:36:43 -0300

    navbar and hero added

---
+++ components/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import Link from 'next/link';
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+
+  return (
+    <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+        <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
+            <Link href='/'>
+                <h1 className='font-bold text-xl'>Templo de Tierra</h1>
+            </Link>
+            <ul className='hidden sm:flex'>
+                <li className='p-4'>
+                    <Link href='/'>El Templo</Link>
+                </li>
+                <li className='p-4'>
+                    <Link href='/'>Retiros y eventos</Link>
+                </li>
+                <li className='p-4'>
+                    <Link href='/'>Reservas</Link>
+                </li>
+                <li className='p-4'>
+                    <Link href='/'>Contacto</Link>
+                </li>
+            </ul>
+
+            {/* Mobile Button */}
+            <div className='block sm:hidden z-10'>
+                {nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />}
+            </div>
+            {/* Mobile Menu */}
+            <div className={nav ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300' : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'}>
+            <ul>
+                <li className='p-4 text-4xl hover:text-gray-500'>
+                    <Link href='/'>El Templo</Link>
+                </li>
+                <li className='p-4 text-4xl hover:text-gray-500'>
+                    <Link href='/'>Retiros y eventos</Link>
+                </li>
+                <li className='p-4 text-4xl hover:text-gray-500'>
+                    <Link href='/'>Reservas</Link>
+                </li>
+                <li className='p-4 text-4xl hover:text-gray-500'>
+                    <Link href='/'>Contacto</Link>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
